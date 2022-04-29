ff202cd6105f2b13d863910f42acc1a7e309939c
naeem <kazinaeemrayhan@gmail.com>
2022-04-29 14:55:23 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// dotenv
+require("dotenv").config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// Mongodb
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lncme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const inventoryCollection = client.db("inventory").collection("product");
+
+    // loading all product
+    app.get("/product", async (req, res) => {
+      const query = {};
+      const cursor = inventoryCollection.find(query);
+      const products = await cursor.toArray();
+      res.send(products);
+    });
+
+    //
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Server Running");
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
