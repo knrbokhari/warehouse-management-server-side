96f197e54ccdff7e3a7ce3f416686b8efbec2dbf
naeem <kazinaeemrayhan@gmail.com>
2022-05-08 23:41:50 +0600

    fix problem

--- index.js
+++ index.js
@@ -13,21 +13,21 @@ app.use(cors());
 app.use(express.json());
 
 // verifyJWT
-function verifyJWT(req, res, next) {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send({ message: "Unauthorized access" });
-  }
-  const token = authHeader.split(" ")[1];
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-    if (err) {
-      return res.status(403).send({ message: "Forbidden access" });
-    }
-    // console.log("decoded", decoded);
-    req.decoded = decoded;
-    next();
-  });
-}
+// function verifyJWT(req, res, next) {
+//   const authHeader = req.headers.authorization;
+//   if (!authHeader) {
+//     return res.status(401).send({ message: "Unauthorized access" });
+//   }
+//   const token = authHeader.split(" ")[1];
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(403).send({ message: "Forbidden access" });
+//     }
+//     // console.log("decoded", decoded);
+//     req.decoded = decoded;
+//     next();
+//   });
+// }
 
 // Mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lncme.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -54,6 +54,7 @@ async function run() {
 
     // // loading all product
     app.get("/product", async (req, res) => {
+      const email = req.query.email;
       const query = { email: email };
       const cursor = inventoryCollection.find(query);
       const products = await cursor.toArray();
